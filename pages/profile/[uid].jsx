99f9d0a1cd76388b80ee44dbf3af3ce0e5053133
Jen <47681735+jlynnova@users.noreply.github.com>
2022-12-14 14:00:23 -0700

    trouble shooting bio

--- pages/profile/[uid].jsx
+++ pages/profile/[uid].jsx
@@ -27,7 +27,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
   const devs = await getDevelopers();
   const dev = devs.find((dev) => dev.uid === params.uid);
-  const bio = { ...dev, heading: generateRandomTitle() };
+  const bio = { ...dev, heading: {type} };
 
   return {
     props: bio,
