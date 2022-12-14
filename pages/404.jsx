aafac92b0dabb089ae3ed0d84d6c8444722c2556
Jen <47681735+jlynnova@users.noreply.github.com>
2022-12-14 13:41:17 -0700

    install react icons, fix link on 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -13,7 +13,7 @@ export default function PageNotFound(){
         
         <div className="mt-10">
         <Link
-                href="#"
+                href="/"
                 className=" items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
                 Go back home
